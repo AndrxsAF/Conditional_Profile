9f3177a87d53102af3598fb1393882ddeb6224b0
AndrxsAF <andrxsaf@gmail.com>
2021-12-18 13:44:41 +0100

    fixed some code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,27 +4,23 @@ const boton = document.querySelector("#todo-add-button")
 
 let lista = []
 
-
 const addToDo = (arr) => {
     let toPrint = ''
     for (i in arr) {
         toPrint += `<li>${arr[i]} <button onclick="eliminar(${i})" id="todo-${i}">Eliminar</button></i>`
     }
     todo.innerHTML = toPrint
-    console.log(toPrint)
-    console.log("impreso")
 }
 
 const handleClick = (event) => {
+    event.preventDefault()
     let additem = texto.value
     lista.push(additem)
     addToDo(lista)
-    console.log(lista)
-    console.log("guardado item")
+    texto.value = ""
 }
 
 const eliminar = (numero) => {
-    // eliminar numero de la lista con un slice, buscar la manera de poder comparar el length de la lista con el index que hay que eliminar
     lista.splice( numero, 1 )
     addToDo(lista)
 }
